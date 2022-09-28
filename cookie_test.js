4e07a58bd5b7fc121a21227c6942ab63580cdb77
PB101010 <62386582+PB101010@users.noreply.github.com>
2022-09-28 18:05:43 +0800

    Update cookie_test.js

--- cookie_test.js
+++ cookie_test.js
@@ -2,9 +2,9 @@
 [rewrite_local]
 https:\/\/some_url_match_rule url script-response-body cookie_test.js
 */
-
+$notify("测试", "test", "重写规则触发");
 cookie = $request.headers['Cookie'];
 console.log("测试cookie获取成功");
 console.log("--------------------------");
 console.log(cookie);
-$notify("测试cookie获取成功");
+$notify("测试", "test", "cookie获取成功");
